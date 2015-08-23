f583750fcb0032bfbb3a850da7b92374765c94cb
OrangeeZ <ultramegacrasher@gmail.com>
2015-08-23 15:14:21 +0300

    + Helicopter

---
+++ Assets/Packages/Utility/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed = 3f;
+
+    private void Update() {
+
+        transform.localRotation *= Quaternion.AngleAxis( speed * Time.deltaTime, Vector3.forward );
+    }
+
+}
