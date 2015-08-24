5c9764643c437d4665620cebfd76b56868848db9
solo1h <reunov.hv@mail.ru>
2015-08-25 02:59:00 +0300

    add soundtreck

---
+++ Assets/Soundtrack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Soundtrack : MonoBehaviour {
+
+	AudioSource fxSound;
+	public AudioClip soundtrack;
+
+	// Use this for initialization
+	void Start () {
+		fxSound = GetComponent<AudioSource> ();
+		fxSound.Play ();
+	}
+}
