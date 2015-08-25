49eb7f550eab79f6997237b95d76b51acf0c6e72
solo1h <reunov.hv@mail.ru>
2015-08-25 03:29:25 +0300

    balance strikes back

--- Assets/Scripts/Gameplay/GameplayController.cs
+++ Assets/Scripts/Gameplay/GameplayController.cs
@@ -13,7 +13,7 @@ public class GameplayController {
 
     public IntReactiveProperty dangerLevel = new IntReactiveProperty( 0 );
 
-    public static int maxDanger = 1000;
+    public static int maxDanger = 250;
 
     public GameplayController() {
 
